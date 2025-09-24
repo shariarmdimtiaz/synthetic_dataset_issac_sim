0ac03e698b42ac3e92eb24fbd960e63ff89406c1
shariarmdimtiaz <shariarmdimtiaz@gmail.com>
2025-09-24 15:07:23 +0900

    set default path

--- RGB.py
+++ RGB.py
@@ -5,3 +5,19 @@ import omni, omni.timeline
 import omni.replicator.core as rep
 import imageio.v2 as iio
 import matplotlib.cm as cm
+
+
+# -------- settings --------
+OUTPUT_DIR   = r"C:\Project\data"
+CAM_PATH     = "/World/Camera"
+OBJ_PATH     = "/World/Neo"
+RESOLUTION   = (1920, 1440)
+FPS          = 30
+FRAME_RANGE  = (0, 120)
+
+
+# ---------- create the output dir --------------
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+rgb_dir   = os.path.join(OUTPUT_DIR, "rgb");   os.makedirs(rgb_dir, exist_ok=True)
+depth_dir = os.path.join(OUTPUT_DIR, "depth"); os.makedirs(depth_dir, exist_ok=True)
+
