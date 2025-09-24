1a6037c824415dddbacac78eda1195add00461ce
shariarmdimtiaz <shariarmdimtiaz@gmail.com>
2025-09-24 14:59:01 +0900

    started projected

---
+++ RGB.py
@@ -0,0 +1,7 @@
+# Isaac Sim Script Editor - Run inside Isaac Sim
+import asyncio, os, csv, numpy as np
+from pxr import Usd, UsdGeom, Gf
+import omni, omni.timeline
+import omni.replicator.core as rep
+import imageio.v2 as iio
+import matplotlib.cm as cm
